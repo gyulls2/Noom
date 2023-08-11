595d176cf8eb7040edae15be188eb48a0d6190bf
gyull <kuri6233@gmail.com>
2023-08-12 02:46:15 +0900

    ✨ 1.8 Nicknames part Two

--- src/server.js
+++ src/server.js
@@ -20,12 +20,23 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
   sockets.push(socket);
+  socket["nickname"] = "익명";
   console.log("Connected to Browser ✅");
   socket.on("close", () => {
     console.log("Disconnected to Browser ❌");
   });
-  socket.on("message", (message) => {
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf-8")));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        );
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
   });
 });
 
